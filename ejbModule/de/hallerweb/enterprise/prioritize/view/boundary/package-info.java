202eb75f3775c2ade0923c2d8a00d344a71c8a0f
Peter Haller <peter.haller@prioritize-iot.de>
2024-01-07 15:02:15 +0100

    Revert "corrected repository directory structure"
    This reverts commit 39d00bb7ba7860f2fe90563eba26566f2a1e8528.

---
+++ ejbModule/de/hallerweb/enterprise/prioritize/view/boundary/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+/**
+ * 
+ * <p>
+ * Copyright: (c) 2015
+ * </p>
+ * <p>
+ * Peter Haller
+ * </p>
+ * 
+ * @author peter
+ */
+package de.hallerweb.enterprise.prioritize.view.boundary;

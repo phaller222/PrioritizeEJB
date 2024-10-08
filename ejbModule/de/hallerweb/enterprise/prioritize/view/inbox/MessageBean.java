6b0d83ea4ce1362865192c75e0a0352aeb711947
Peter Haller <peter.haller@prioritize-iot.de>
2024-10-09 01:55:19 +0200

    Changes on MessageController

--- ejbModule/de/hallerweb/enterprise/prioritize/view/inbox/MessageBean.java
+++ ejbModule/de/hallerweb/enterprise/prioritize/view/inbox/MessageBean.java
@@ -20,8 +20,6 @@ import de.hallerweb.enterprise.prioritize.controller.security.SessionController;
 import de.hallerweb.enterprise.prioritize.controller.security.UserRoleController;
 import de.hallerweb.enterprise.prioritize.model.inbox.Message;
 import de.hallerweb.enterprise.prioritize.model.security.User;
-import org.jboss.resteasy.logging.Logger;
-
 import jakarta.annotation.PostConstruct;
 import jakarta.ejb.EJB;
 import jakarta.enterprise.context.SessionScoped;
@@ -30,6 +28,8 @@ import jakarta.faces.context.ExternalContext;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import org.jboss.resteasy.logging.Logger;
+
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -43,158 +43,158 @@ import java.util.List;
  * <p>
  * Peter Haller
  * </p>
- * 
+ *
  * @author peter
  */
 @Named
 @SessionScoped
 public class MessageBean implements Serializable {
 
-	private static final String NAVIGATION_MESSAGES = "messages";
-	
-	@Inject
-	SessionController sessionController;
-	@EJB
-	transient MessageController controller;
-	@EJB
-	transient UserRoleController userRoleController;
+    private static final String NAVIGATION_MESSAGES = "messages";
 
-	transient List<Message> messages; // List of messages
-	String message; // Stores the newly composed Message
-	String subject; // Stores the newly composed Message subject
-	String readMessageSubject; // Subject of currently viewed message
-	String readMessageMessage; // Message of currently viewed message
-	String to; // Stores the User to receive the message
-	transient List<User> recipientList; // List of Users in the system.
-	String readMessageId; // ID of currently viewed message
+    @Inject
+    transient SessionController sessionController;
+    @EJB
+    transient MessageController controller;
+    @EJB
+    transient UserRoleController userRoleController;
 
-	@Named
-	public String getMessage() {
-		return message;
-	}
+    transient List<Message> messages; // List of messages
+    String message; // Stores the newly composed Message
+    String subject; // Stores the newly composed Message subject
+    String readMessageSubject; // Subject of currently viewed message
+    String readMessageMessage; // Message of currently viewed message
+    String to; // Stores the User to receive the message
+    transient List<User> recipientList; // List of Users in the system.
+    String readMessageId; // ID of currently viewed message
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    @Named
+    public String getMessage() {
+        return message;
+    }
 
-	@Named
-	public String getSubject() {
-		return subject;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+    @Named
+    public String getSubject() {
+        return subject;
+    }
 
-	@Named
-	public List<Message> getMessages() {
-		return controller.getReceivedMessages(sessionController.getUser());
-	}
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-	@Named
-	public List<Message> getSentMessages() {
-		return controller.getSentMessages(sessionController.getUser());
-	}
+    @Named
+    public List<Message> getMessages() {
+        return controller.getReceivedMessages(sessionController.getUser());
+    }
 
-	public String getReadMessageSubject() {
-		return readMessageSubject;
-	}
+    @Named
+    public List<Message> getSentMessages() {
+        return controller.getSentMessages(sessionController.getUser());
+    }
 
-	@Named
-	public String getReadMessageMessage() {
-		return readMessageMessage;
-	}
+    public String getReadMessageSubject() {
+        return readMessageSubject;
+    }
 
-	@Named
-	public String readMessage(String readMessageId) {
-		this.readMessageId = readMessageId;
-		Message readMessage = controller.findMessageById(Integer.parseInt(readMessageId));
-		this.readMessageSubject = readMessage.getSubject();
-		this.readMessageMessage = readMessage.getContent();
-		return "readmessage";
-	}
+    @Named
+    public String getReadMessageMessage() {
+        return readMessageMessage;
+    }
 
-	@Named
-	public User getCurrentUser() {
-		return sessionController.getUser();
-	}
+    @Named
+    public String readMessage(String readMessageId) {
+        this.readMessageId = readMessageId;
+        Message readMessage = controller.findMessageById(Integer.parseInt(readMessageId));
+        this.readMessageSubject = readMessage.getSubject();
+        this.readMessageMessage = readMessage.getContent();
+        return "readmessage";
+    }
 
-	@PostConstruct
-	public void init() {
-		to = "";
-	}
+    @Named
+    public User getCurrentUser() {
+        return sessionController.getUser();
+    }
 
-	@Produces
-	@Named
-	public List<User> getRecipiantList() {
-		return userRoleController.getAllUsers(sessionController.getUser());
-	}
+    @PostConstruct
+    public void init() {
+        to = "";
+    }
 
-	public List<String> completeRecipiantList(String query) {
-		List<String> users = userRoleController.getAllUserNames(sessionController.getUser());
-		List<String> result = new ArrayList<>();
-		for (String username : users) {
-			if (username.startsWith(query)) {
-				result.add(username);
-			}
-		}
-		return result;
+    @Produces
+    @Named
+    public List<User> getRecipiantList() {
+        return userRoleController.getAllUsers(sessionController.getUser());
+    }
 
-	}
+    public List<String> completeRecipiantList(String query) {
+        List<String> users = userRoleController.getAllUserNames(sessionController.getUser());
+        List<String> result = new ArrayList<>();
+        for (String username : users) {
+            if (username.startsWith(query)) {
+                result.add(username);
+            }
+        }
+        return result;
 
-	@Named
-	public String getTo() {
-		return to;
-	}
+    }
 
-	@Named
-	public void setTo(String user) {
-		this.to = user;
-	}
+    @Named
+    public String getTo() {
+        return to;
+    }
 
-	@Named
-	public String delete(Message msg) {
-		controller.deleteMessage(msg.getId());
-		init();
-		return NAVIGATION_MESSAGES;
-	}
+    @Named
+    public void setTo(String user) {
+        this.to = user;
+    }
 
-	@Named
-	public String sendMessage() {
-		controller.createMessage(sessionController.getUser(), userRoleController.findUserByUsername(this.to,sessionController.getUser()).getUsername(),
-				this.subject, this.message);
-		return NAVIGATION_MESSAGES;
-	}
+    @Named
+    public String delete(Message msg) {
+        controller.deleteMessage(msg.getId());
+        init();
+        return NAVIGATION_MESSAGES;
+    }
 
-	@Named
-	public String overview() {
-		return NAVIGATION_MESSAGES;
-	}
+    @Named
+    public String sendMessage() {
+        controller.createMessage(sessionController.getUser(), userRoleController.findUserByUsername(this.to, sessionController.getUser()).getUsername(),
+                this.subject, this.message);
+        return NAVIGATION_MESSAGES;
+    }
 
-	@Named
-	public String inbox() {
-		return "inbox";
-	}
+    @Named
+    public String overview() {
+        return NAVIGATION_MESSAGES;
+    }
 
-	@Named
-	public void gotoInbox() {
-		ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-		try {
-			context.redirect(context.getApplicationContextPath() + "/client/messages/inbox.xhtml");
-		} catch (IOException e) {
-			Logger.getLogger(getClass()).error(e.getMessage());
-		}
-	}
+    @Named
+    public String inbox() {
+        return "inbox";
+    }
 
-	@Named
-	public void setMessageRead() {
-		Message msg = controller.findMessageById(Integer.parseInt(readMessageId));
-		controller.setMessageRead(msg, true);
-	}
+    @Named
+    public void gotoInbox() {
+        ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
+        try {
+            context.redirect(context.getApplicationContextPath() + "/client/messages/inbox.xhtml");
+        } catch (IOException e) {
+            Logger.getLogger(getClass()).error(e.getMessage());
+        }
+    }
 
-	@Named
-	public List<Message> getUnreadMessages() {
-		return controller.getUnreadMessages(sessionController.getUser());
-	}
+    @Named
+    public void setMessageRead() {
+        Message msg = controller.findMessageById(Integer.parseInt(readMessageId));
+        controller.setMessageRead(msg, true);
+    }
+
+    @Named
+    public List<Message> getUnreadMessages() {
+        return controller.getUnreadMessages(sessionController.getUser());
+    }
 
 }
